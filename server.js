957b86edc9ea9bf65c07c2d68a1be015ce5deef6
Oubaid Maudarbux <om357@live.mdx.ac.uk>
2025-10-04 15:26:31 +0400

    Added server.js to combine routes and middleware and to create connection to MongoDB

---
+++ server.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import logger from './middleware/logger.js';
+import staticMw from './middleware/static.js';
+import lessonRoutes from './routes/lessonRoutes.js';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(logger);
+app.use('/images', staticMw);
+
+MongoClient.connect(process.env.MONGO_URI)
+  .then(client => {
+    app.locals.db = client.db('afterSchool');
+    app.listen(process.env.PORT, () => console.log('API on', process.env.PORT));
+  })
+  .catch(err => console.error(err));
+
+app.use('/lessons', lessonRoutes);
